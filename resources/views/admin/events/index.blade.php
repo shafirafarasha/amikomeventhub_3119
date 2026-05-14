55c76a8d8d4123e3406af0d1420f99948153a567
shafirafarasha <shafirafarasha@example.com>
2026-05-14 15:24:07 +0700

    Fix poster image path

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -71,8 +71,8 @@ transition">
                                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0
-        01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4
-        7h16"></path>
+            01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4
+            7h16"></path>
                                             </svg>
                                         </button>
                                     </form>
@@ -83,13 +83,13 @@ text-indigo-600 rounded-xl hover:bg-indigo-600 hover:text-white transition">
                                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 00-2
-        2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828
-        15H9v-2.828l8.586-8.586z"></path>
+            2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828
+            15H9v-2.828l8.586-8.586z"></path>
                                         </svg>
                                     </a>
                                     <!-- Catatan Modul: Deretan tombol fitur modifikasi
 
-        (U dan D)
+            (U dan D)
     akan ditanamkan pada tahap berikutnya -->
 
                                 </div>
